2fa106c4327e25b26d58107af94df66cc04a2cb0
Yasenbaka <1260352143@qq.com>
2025-11-10 08:28:20 +0800

    修改主Header垂直不居中问题

--- src/components/HeaderFooter/HeaderComp.vue
+++ src/components/HeaderFooter/HeaderComp.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import "@/style/Header.css";
 import {onMounted, ref} from "vue";
 import type {CategoryImpl} from "@/interfaces/CategoryImpl.ts";
 import {baseHttp} from "@/utils/https.ts";
@@ -54,58 +55,5 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  position: relative; /* 定位参考基准 */
-  height: 70px;
-  background-color: #ffffff70;
-  box-shadow: 0 0 0.1rem 0.1rem rgb(0 0 0 / 10%);
-  overflow: hidden;
-  padding: 10px;
-  padding-inline: 30px;
-  transition: all 0.5s;
-  color: #000000;
-}
-.header:hover {
-  background-color:  #ffffff;
-}
 
-
-/* -- menu-container style -- */
-.menu-container {
-
-
-  .menu-item {
-    margin-inline: 10px;
-    font-family: "HarmonyOS Sans", sans-serif;
-    color: #000000;
-    text-decoration: none;
-    font-size: 18px;
-    cursor: pointer;
-    /* 确保不影响点击事件 */
-    pointer-events: auto;
-    /* 禁止用户选中 */
-    user-select: none;
-    /* 下划线样式 */
-    background: linear-gradient(to right, #9b9b9b, #9b9b9b) no-repeat right bottom;
-    background-size: 0 2px;
-    transition: background-size .2s;
-
-  }
-
-  .menu-item:hover {
-    background-position: left bottom;
-    background-size: 100% 2px;
-  }
-
-  /* 被激活时的样式 */
-
-  .menu-item.active {
-    /* 下划线样式 */
-    background: linear-gradient(to right, #9b9b9b, #9b9b9b) no-repeat right bottom;
-    background-size: auto 2px;
-  }
-}
 </style>
